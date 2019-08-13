609a9002095a53cbd3a384b28f744c7259b94241
anyue967 <anyue1993@gmail.com>
2019-08-13 13:54:56 +0800

    add k8s fiels

---
+++ docker/k8s/kubernetes-ha-kubeadm/README.md
@@ -0,0 +1,12 @@
+# kubernetes-ha-kubeadm - 1.14
+> 完整视频课程请移步慕课网的实战课：**[《Kubernetes生产级实践指南 从部署到核心应用》][1]**  
+> 课程对应git地址： **https://git.imooc.com/coding-335/kubernetes-ha-kubeadm**
+
+## 项目介绍
+项目致力于让有意向使用原生kubernetes集群的企业或个人，可以方便的、系统的使用**kubeadm**的方式搭建kubernetes高可用集群。并且让相关的人员可
+以更好的理解kubernetes集群的运作机制。
+- **集群部署过程严格按照官方文档的流程。**
+- **非科学上网的同学同样适用。**
+- **持续跟进kubernetes最新版本**
+
+[1]:https://coding.imooc.com/class/335.html
---
+++ docker/k8s/kubernetes-ha-kubeadm/init.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+target=target
+declare -A kvs=()   # 声明数组
+
+function replace_files() {
+    local file=$1
+    if [ -f $file ]; then
+        echo "$file"
+        for key in ${!kvs[@]}
+        do
+            value=${kvs[$key]}
+            value=${value//\//\\\/}
+            sed -i "s/{{$key}}/${value}/g" $file
+        done
+        return 0
+    fi
+    if [ -d $file ]; then
+        for f in `ls $file`
+        do
+            replace_files "${file}/${f}"
+        done
+    fi
+    return 0
+}
+
+rm -fr $target
+mkdir -p $target
+
+cp -r configs $target
+cp -r scripts $target
+cp -r addons $target
+cd $target      # 切进目标生成目录, 进行文件替换
+
+echo "====替换变量列表===="
+while read line;do
+    if [ "${line:0:1}" == "#" -o "${line:0:1}" == "" ];then
+        continue;
+    fi
+    key=${line/=*/}
+    value=${line#*=}
+    echo "$key=$value"
+    kvs["$key"]="$value"
+done < ../global-config.properties
+
+echo -e "\n====替换脚本===="
+replace_files scripts
+
+echo -e "\n====替换配置文件===="
+DIR=${kvs["MASTER_0_IP"]}
+mkdir $DIR
+cp -r ./configs/kubeadm-config.yaml $DIR
+kvs["HOST_IP"]=$DIR
+kvs["HOST_NAME"]=${kvs["MASTER_0_HOSTNAME"]}
+replace_files $DIR
+
+DIR=${kvs["MASTER_1_IP"]}
+mkdir $DIR
+cp -r ./configs/kubeadm-config.yaml $DIR
+kvs["HOST_IP"]=$DIR
+kvs["HOST_NAME"]=${kvs["MASTER_1_HOSTNAME"]}
+replace_files $DIR
+
+DIR=${kvs["MASTER_2_IP"]}
+mkdir $DIR
+cp -r ./configs/kubeadm-config.yaml $DIR
+kvs["HOST_IP"]=$DIR
+kvs["HOST_NAME"]=${kvs["MASTER_2_HOSTNAME"]}
+replace_files $DIR
+rm -rf ./configs/kubeadm-config.yaml
+
+replace_files addons
+
+echo "配置生成成功，位置: `pwd`"
